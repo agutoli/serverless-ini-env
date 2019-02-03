1da2dc2929ac9a18c962adbdff697c0e463a41a3
Bruno Agutoli <bruno.agutoli@gmail.com>
2019-02-03 17:47:28 -0200

    improvements

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-## WIP
+# serverless-ini-env
+[![serverless](http://public.serverless.com/badges/v3.svg)](http://www.serverless.com)
+
+## Install
+
+`npm install --save-dev serverless-ini-env`
+
+Add the plugin to your `serverless.yml` file:
+
+```yaml
+plugins:
+  - serverless-ini-env
+```
+
+## Configuration
+```yaml
+custom:
+  serverless-ini-env:
+    dev: "./you_configs/dev.ini"
+    prod: "./you_configs/prod.ini"
+```
+
+## Ini File example
+
+`./you_configs/dev.ini`
+
+```ini
+# will be available for both functions
+MY_GLOBAL_VAR=DEV_VALUE
+
+[my_function_name_a]
+  NAME="function A"
+  FOO=DEV_VALUE
+
+[my_function_name_b]
+  NAME="function B"
+  BAR=DEV_VALUE
+```
+
+`./you_configs/prod.ini`
+
+```ini
+# will be available for both functions
+MY_GLOBAL_VAR=PROD_VALUE
+
+[my_function_name_a]
+  NAME="function A"
+  FOO=PROD_VALUE
+
+[my_function_name_b]
+  NAME="function B"
+  BAR=PROD_VALUE
+```
+
+
+## Contributing
+
+Yes, thank you!
+This plugin is community-driven, most of its features are from different authors.
+Please update the docs and tests and add your name to the package.json file.
+We try to follow [Airbnb's JavaScript Style Guide](https://github.com/airbnb/javascript).
+
+## License
+
+MIT
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 const BbPromise = require('bluebird');
-const fs = require('fs')
-const path = require('path')
-const ini = require('ini')
+const fs = require('fs');
+const path = require('path');
+const ini = require('ini');
 
 class ServerlessIniEnv {
   constructor(serverless, options) {
-    this.serverless = serverless
+    this.serverless = serverless;
     this.provider = this.serverless.getProvider('aws');
-    this.options = options
+    this.options = options;
 
     this.options.stage = options.stage || 'dev';
     this.options.region = this.provider.getRegion();
 
-    const inboundSettings = (serverless.service.custom || {})['serverless-ini-env']
+    const inboundSettings = (serverless.service.custom || {})['serverless-ini-env'];
 
     const defaultSettings = {
       [this.options.stage]: path.join(process.env.PWD, `${this.options.stage}.ini`)
-    }
+    };
 
-    this.settings = Object.assign({}, defaultSettings, inboundSettings)
+    this.settings = Object.assign({}, defaultSettings, inboundSettings);
 
     this.commands = {
       'update-environments': {
@@ -52,60 +52,64 @@ class ServerlessIniEnv {
       },
     };
 
-    this.loadEnvironments(this.settings[this.options.stage])
+    this.loadEnvironments(this.settings[this.options.stage]);
 
     this.hooks = {
-      'update-environments:function:init': () => BbPromise.bind(this).then(() => {
-        return this.updateSingleFunction(this.settings[this.options.stage])
-      }),
-      'update-environments:init': () => BbPromise.bind(this).then(() => {
-        return this.updateAllFunctions(this.settings[this.options.stage])
-      })
-    }
+      'update-environments:function:init': () => BbPromise.bind(this)
+        .then(() => this.updateSingleFunction(this.settings[this.options.stage])),
+      'update-environments:init': () => BbPromise.bind(this)
+        .then(() => this.updateAllFunctions(this.settings[this.options.stage]))
+    };
   }
 
   loadFile(filename) {
     try {
-      return fs.readFileSync(filename, 'utf-8')
-    } catch(e) {
-      this.serverless.cli.log(`can not find config file "${filename}"`)
-      throw new Error("error")
+      return fs.readFileSync(filename, 'utf-8');
+    } catch (e) {
+      this.serverless.cli.log(`can not find config file "${filename}"`);
+      throw new Error('error');
     }
   }
 
   loadConfigEnvs(filename) {
-    const config = ini.parse(this.loadFile(filename))
+    const config = ini.parse(this.loadFile(filename));
 
-    let globalEnvs = {}
-    for (let key in config) {
-      if (typeof config[key] === 'string') {
-        globalEnvs[key] = config[key]
+    const globalEnvs = {};
+    for (const key in config) {
+      switch (typeof config[key]) {
+        case 'string':
+        case 'boolean':
+        case 'number':
+          globalEnvs[key] = config[key];
+          break;
       }
     }
 
-    let environments = {}
-    for (let key in config) {
-      if (typeof config[key] != 'string') {
-        environments[key] = {...globalEnvs, ...config[key] }
+    const environments = {};
+    for (const key in config) {
+      if (typeof config[key] === 'object') {
+        environments[key] = { ...globalEnvs, ...config[key] };
       }
     }
 
-    return environments
+    return environments;
   }
 
   getFunctionName(ns) {
-    const service = this.serverless.service.service
-    return `${service}-${this.options.stage}-${ns}`
+    const service = this.serverless.service.service;
+
+    return `${service}-${this.options.stage}-${ns}`;
   }
 
   mergeConfig(ns, config) {
-    const environment = ((this.serverless.service.functions[ns] || {}).environment || {})
-    return Object.assign({}, environment, config[ns])
+    const environment = ((this.serverless.service.functions[ns] || {}).environment || {});
+
+    return Object.assign({}, environment, config[ns]);
   }
 
   async updateSingleFunction(filename) {
-    const config = this.loadConfigEnvs(filename)
-    const FunctionName = this.getFunctionName(this.options.f)
+    const config = this.loadConfigEnvs(filename);
+    const FunctionName = this.getFunctionName(this.options.f);
 
     const params = {
       FunctionName,
@@ -115,59 +119,62 @@ class ServerlessIniEnv {
     };
 
     try {
-      await this.provider.request('Lambda', 'updateFunctionConfiguration', params)
-      this.serverless.cli.log(`[${FunctionName}] - Updating environments... OK`)
-    } catch(e) {
-      console.error(e)
-      this.serverless.cli.log(`[${FunctionName}] - Updating environments... Error`)
+      await this.provider.request('Lambda', 'updateFunctionConfiguration', params);
+      this.serverless.cli.log(`[${FunctionName}] - Updating environments... OK`);
+    } catch (e) {
+      console.error(e);
+      this.serverless.cli.log(`[${FunctionName}] - Updating environments... Error`);
     }
 
-    return true
+    return true;
   }
 
   async updateAllFunctions(filename) {
-    const config = this.loadConfigEnvs(filename)
+    const config = this.loadConfigEnvs(filename);
 
-    for(let ns in config) {
-      const FunctionName = this.getFunctionName(ns)
+    for (const ns in config) {
+      const FunctionName = this.getFunctionName(ns);
       const params = {
         FunctionName,
         Environment: {
           Variables: this.mergeConfig(ns, config)
         }
       };
+
       try {
-        await this.provider.request('Lambda', 'updateFunctionConfiguration', params)
-        this.serverless.cli.log(`[${FunctionName}] - Updating environments... OK`)
-      } catch(e) {
-        console.error(e)
-        this.serverless.cli.log(`[${FunctionName}] - Updating environments... Error`)
+        this.provider.request('Lambda', 'updateFunctionConfiguration', params).then(() => {
+          this.serverless.cli.log(`[${FunctionName}] - Updating environments... OK`);
+        });
+      } catch (e) {
+        console.error(e);
+        this.serverless.cli.log(`[${FunctionName}] - Updating environments... Error`);
       }
     }
 
-    return true
+    return true;
   }
 
   async loadEnvironments(filename) {
-    const config = this.loadConfigEnvs(filename)
+    const config = this.loadConfigEnvs(filename);
 
-    for (let ns in config) {
-      const envs = this.mergeConfig(ns, config)
-      const counts = Object.keys(envs).length
-      this.serverless.cli.log(`[${ns}] - loading environments... (${counts} ${counts === 1? 'var': 'vars'})`)
+    for (const ns in config) {
+      const envs = this.mergeConfig(ns, config);
+      const counts = Object.keys(envs).length;
+
+      this.serverless.cli.log(`[${ns}] - loading environments... (${counts} ${counts === 1 ? 'var' : 'vars'})`);
 
       if (!this.serverless.service.functions[ns]) {
-        throw new Error(`function ${ns} does not exists!`)
+        throw new Error(`function ${ns} does not exists!`);
       }
 
-      this.serverless.service.functions[ns].environment = envs
+      this.serverless.service.functions[ns].environment = envs;
     }
 
     setTimeout(() => {
-      this.serverless.cli.log(`IniEnv config: "${filename}", stage: ${this.options.stage}`)
-    }, 3000)
+      this.serverless.cli.log(`IniEnv config: "${filename}", stage: ${this.options.stage}`);
+    }, 3000);
 
-    return true
+    return true;
   }
 }
 
